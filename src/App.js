59ea48c5751eb9d1409d100cfb623a582a85654c
Jeff Bliss <jeff.a.bliss@gmail.com>
2020-03-12 10:01:54 -0400

    Cleaned up some code

--- src/App.js
+++ src/App.js
@@ -4,11 +4,6 @@ import axios from 'axios';
 import './App.css';
 
 const API_URL = 'https://pwol6zjt3f.execute-api.us-east-1.amazonaws.com/production/landat-ndvi?lng=-85.01358032226562&lat=40.36224194899873'
-//let rawDataYearOne = ['20000108,23', '20000116,22', '20000124,23', '20000201,23', '20000209,23', '20000217,23', '20000225,24', '20000305,23', '20000313,22', '20000321,21', '20000329,23', '20000406,23', '20000414,25', '20000422,24', '20000430,25', '20000508,31', '20000516,34', '20000524,37', '20000601,36', '20000609,36', '20000617,37', '20000625,44', '20000703,53', '20000711,60', '20000719,68', '20000727,74', '20000804,81', '20000812,87', '20000820,89', '20000828,90', '20000905,88', '20000913,86', '20000921,83', '20000929,79', '20001007,72', '20001015,62', '20001023,51', '20001031,41', '20001108,34', '20001116,29', '20001124,26', '20001202,24', '20001210,24', '20001218,23', '20001226,23', '20010103,22'];
-//let rawDataYearTwo = ["20010108,22", "20010116,22", "20010124,22", "20010201,21", "20010209,22", "20010217,21", "20010225,22", "20010305,21", "20010313,23", "20010321,23", "20010329,25", "20010406,25", "20010414,27", "20010422,29", "20010430,31", "20010508,33", "20010516,35", "20010524,37", "20010601,38", "20010609,39", "20010617,42", "20010625,43", "20010703,46", "20010711,48", "20010719,52", "20010727,61", "20010804,72", "20010812,81", "20010820,88", "20010828,92", "20010905,92", "20010913,91", "20010921,89", "20010929,83", "20011007,74", "20011015,64", "20011023,54", "20011031,46", "20011108,39", "20011116,34", "20011124,32", "20011202,30", "20011210,27", "20011218,27", "20011226,25", "20020103,24"];
-//let rawDataYearThree = ["20020108,23", "20020116,22", "20020124,22", "20020201,22", "20020209,22", "20020217,22", "20020225,23", "20020305,23", "20020313,23", "20020321,25", "20020329,24", "20020406,23", "20020414,23", "20020422,23", "20020430,25", "20020508,29", "20020516,30", "20020524,30", "20020601,33", "20020609,35", "20020617,37", "20020625,40", "20020703,45", "20020711,51", "20020719,59", "20020727,64", "20020804,69", "20020812,73", "20020820,76", "20020828,78", "20020905,78", "20020913,74", "20020921,67", "20020929,62", "20021007,54", "20021015,47", "20021023,40", "20021031,36", "20021108,31", "20021116,28", "20021124,27", "20021202,26", "20021210,26", "20021218,27", "20021226,25", "20030103,25"];
-// lng: -85.01358032226562
-// lat:  40.36224194899873
 
 const getDayOfYear = date => {
   var start = new Date(date.getFullYear(), 0, 0);
@@ -18,46 +13,26 @@ const getDayOfYear = date => {
   return day;
 }
 
-/*function parseRawData(data) {
-  return data.map(d => {
-    let split = d.split(',')
-    let dateString = split[0];
-    let val = parseFloat(split[1]);
-    let date = new Date(
-      parseInt(dateString.substring(0, 4)), // year
-      (parseInt(dateString.substring(4, 6)) - 1), // months are 0-indexed
-      parseInt(dateString.substring(6, 8))); // day of month
-    return {date: date, val: val };
-  })
-}*/
-
 function parseRawData(data) {
-  let newData = data.map(d => {
-    let split = d.split(',')
-    let dateString = split[0];
-    let val = parseFloat(split[1]);
-    let year = parseInt(dateString.substring(0, 4));
+  let year_data = {}
+  let counter = 0 // counter is so we can get the first date of the following year
+  data.forEach(function (item, index){
+    //console.log(item)
+    counter++
+    let split = item.split(',')
+    let dateString = split[0]
+    let val = parseFloat(split[1])
+    let year = parseInt(dateString.substring(0, 4))
     let date = new Date(
       parseInt(dateString.substring(0, 4)), // year
       (parseInt(dateString.substring(4, 6)) - 1), // months are 0-indexed
-      parseInt(dateString.substring(6, 8))); // day of month
-    return {
-      date: date,
-      val: val,
-      year: year
-    }
-  })
-  let year_data = {}
-  let counter = 0
-  newData.forEach(function (item, index) {
-    counter++
-    let year = item.year
+      parseInt(dateString.substring(6, 8))) // day of month
     year_data[year] = year_data[year] || []
-    if (counter === 46) {
-      year_data[year - 1].push({ val: item['val'], date: item['date'] })
+    if (counter === 46) { // 46 entries per year (e.g. 45 in 2000 and 1 in 2001)
+      year_data[year - 1].push({ val: val, date: date })
       counter = 0
     } else {
-      year_data[year].push({ val: item['val'], date: item['date'] })
+      year_data[year].push({ val: val, date: date })
     }
   })
   return year_data
@@ -74,10 +49,6 @@ function unpack(data, key) {
     return data.map(d => d.date);
   }
 }
-
-//const parsedDataYearOne = parseRawData(rawDataYearOne)
-//const parsedDataYearTwo = parseRawData(rawDataYearTwo)
-//const parsedDataYearThree = parseRawData(rawDataYearThree)
 
 /*const dataPlotly = [
   { // 2000 data
@@ -250,15 +221,5 @@ class App extends Component {
   }
 
 }
-
-/*function App() {
-  return (
-    <Plot
-      data={dataPlotly}
-      layout={layout}
-      config={{responsive: true}}
-    />
-  );
-}*/
 
 export default App;
